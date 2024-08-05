5dfda37fd4438fa8ae05094ddabf87868923867d
PuuGoo <puugoo002@gmail.com>
2024-08-06 05:40:58 +0700

    20240806 05:40 AM Checkbox and Voucher

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
-import { Component, Renderer2, inject, signal } from '@angular/core';
-import { Order, Product } from '../db';
+import {Component, Renderer2, computed, inject, signal } from '@angular/core';
+import { Order, Product, Voucher } from '../db';
 import { OrderService } from '../services/order.service';
 import {
   CommonModule,
@@ -12,11 +12,23 @@ import { Router, RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import localeVi from '@angular/common/locales/vi';
 registerLocaleData(localeVi);
-import express from 'express';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { VoucherService } from '../services/voucher.service';
+
+export interface Subtask {
+  completed: boolean,
+  order: Order
+}
+
+export interface Task {
+  completed: boolean,
+  subtasks?: Array<Subtask>
+}
+
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
@@ -28,8 +40,15 @@ export class CartComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   idOrder: number = -1;
   totalPrice = 0;
-  subtasks: any = [];
+  task = signal<Task>({
+    completed: false,
+    subtasks: []
+  });
+  vouchers: Voucher[] = [];
+  voucherService: VoucherService = inject(VoucherService);
+  voucher: number = 0;
 
+  // Chức năng xóa giỏ hàng
   deleteCart(event: any) {
     this.idOrder = event.target?.id;
 
@@ -45,28 +64,100 @@ export class CartComponent {
     });
   }
 
+
+  // Xử lý checkbox 
+  // Hiển thị trạng thái click 1 trong các thành phần con
+  partiallyCompleted = () => {
+    let task = this.task();
+    if(!task.subtasks) {
+      return false
+    }
+
+    // console.log(task.subtasks.some((t) => t.completed));
+    
+
+    return task.subtasks.some((t) => t.completed) && !task.subtasks.every(t => t.completed)
+  }
+  // Cập nhật trạng thái checkbox
+  update(event: any, index?: number) {
+    let completed = event.target.checked;
+    console.log(completed);
+    
+    if(index === undefined) {
+      this.task().completed = completed;
+      this.task().subtasks?.forEach(t => t.completed = completed)
+      console.log(this.task().subtasks);
+    } else {
+      this.task().subtasks![index].completed = completed;
+      this.task().completed = this.task().subtasks?.every(t => t.completed) ?? true
+    }
+
+    this.calTotalPrice();
+    console.log(this.totalPrice);
+    
+  }
+  // Show/hide bảng tóm tắt
+  summary() {
+    return this.task().subtasks?.some(t => t.completed) ?? true
+  }
+
+
+  // Xử lý tăng giảm số lượng
+  // Chức năng tăng số lượng
+  incrementQuantity(index: number) {
+    this.task().subtasks![index].order.quantity +=  1;  
+    this.calTotalPrice();
+    this.orderService.updateOrder(this.task().subtasks![index].order.id, this.task().subtasks![index].order.quantity).subscribe(res => {
+      console.log('Update quantity successfully!');
+      
+    })
+  }
+  // Chức năng giảm số lượng
+  decrementQuantity(index: number) {
+    this.task().subtasks![index].order.quantity -=  1;  
+    this.calTotalPrice();
+    this.orderService.updateOrder(this.task().subtasks![index].order.id, this.task().subtasks![index].order.quantity).subscribe(res => {
+      console.log('Update quantity successfully!');
+      
+    })
+  }
+
+  // Tính tổng giá tiền
+  calTotalPrice() {
+    this.totalPrice = 0;
+    this.task().subtasks?.filter(t => t.completed).forEach(t => {
+      this.totalPrice += t.order.quantity * t.order.idProd.price;
+    }) 
+  }
+
+  applyVoucher: FormGroup = new FormGroup({
+    voucherValue: new FormControl(null)
+  })
+
+  submitVoucher() {
+    let voucherCode = this.applyVoucher.value.voucherValue;
+    this.voucher = this.vouchers.filter(v => v.name === voucherCode)[0]?.value ?? 0;
+    return this.voucher;        
+  }
+  
+
   constructor(private router: Router, private render: Renderer2) {
     this.orderService.getAllOrder().then((orders) => {
       this.orders = orders;
-      this.orderService.orders = orders;
-      this.orders.map((e) => {
-        this.totalPrice += e.idProd?.price * e.quantity;
-        // console.log(e.idProd.price);
-        // console.log(e.quantity);
-        // console.log(this.totalPrice);
-      });
-
-      let task: Array<Object> = [];
-      
+      this.orderService.orders = orders;     
       this.orders.forEach((e) => {
-        task.push(Object.assign({
-          ...e,
+        this.task().subtasks?.push({
+          order: e,
           completed: false
-        }))
+        })
       })
-
-      console.log(task);
-      
+      console.log(this.task().subtasks);                
     });
+
+    this.voucherService.getAllVoucheres().subscribe(res => {
+      this.vouchers = Object.assign(res);
+      console.log("Lấy tất cả voucher thành công");
+      console.log(res);        
+    })
   }
 }
--- src/app/services/voucher.service.ts
+++ src/app/services/voucher.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoucherService {
+  url = "http://localhost:3000/voucher"
+  getAllVoucheres() {
+    return this.http.get(this.url);
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
